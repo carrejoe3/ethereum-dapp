77de50b664f2e5ebc588b8935d16ce2dda39af7b
Joe <carre.joe3@gmail.com>
2019-07-21 21:33:30 +0100

    removed default contract and migration script and changed compiler version

---
+++ contracts/Election.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.15;
+
+contract Election {
+    string public candidate;
+    // Constructer
+   function Election () public {
+       candidate = "Candidate 1";
+   }
+}
--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,11 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
+  compilers: {
+    solc: {
+      version: "^0.4.15"
+    }
+  },
   networks: {
     development: {
       host: "127.0.0.1",
