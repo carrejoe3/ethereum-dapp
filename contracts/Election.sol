473810cc8db4e95c87f982ec851a8bb2c5e078f1
Joe <carre.joe3@gmail.com>
2019-07-21 22:03:12 +0100

    More detailed candidate constructor
    -constructor is now setup to create an id, name and vote count
    -number of candidates are also stored in candidateCount

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,28 @@
 pragma solidity ^0.4.15;
 
 contract Election {
-    string public candidate;
+    // Model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    // This is handy because theres no .length in solidity
+    uint public candidatesCount;
+
     // Constructer
    function Election () public {
-       candidate = "Candidate 1";
+       addCandidate("Candidate 1");
+       addCandidate("Candidate 2");
+   }
+
+   function addCandidate (string _name) private {
+       candidatesCount ++;
+       candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
    }
 }
